8f525e2730e75fd19e45cc7fed25571f5840d235
Vincenzo <149695389+ColoVinc@users.noreply.github.com>
2026-04-24 17:06:16 +0200

    Update class-openai.php

--- includes/connectors/class-openai.php
+++ includes/connectors/class-openai.php
@@ -109,7 +109,7 @@ class SiteGenie_OpenAI extends SiteGenie_API_Connector {
                 $tool_args = json_decode( $tc['function']['arguments'], true ) ?? [];
                 $tool_result = SiteGenie_Tools::execute( $tool_name, $tool_args );
 
-                if ( in_array( $tool_name, [ 'create_post', 'update_post', 'delete_post', 'create_custom_post', 'update_custom_post', 'moderate_comment', 'reply_comment', 'update_site_settings', 'create_user', 'create_product', 'add_menu_item' ] ) ) {
+                if ( in_array( $tool_name, [ 'create_post', 'update_post', 'delete_post', 'create_custom_post', 'update_custom_post', 'moderate_comment', 'reply_comment', 'update_site_settings', 'create_user', 'create_product', 'add_menu_item', 'create_component' ] ) ) {
                     $last_action = [ 'tool' => $tool_name, 'result' => $tool_result ];
                 }
 
